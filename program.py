2048fa71e075c49e526ea1bd0c8e85d3f522e6ef
KRYSTOF SENFELD <k.senfeld.20@abdn.ac.uk>
2021-05-26 12:16:51 +0000

    Working logic

--- program.py
+++ program.py
@@ -1,11 +1,13 @@
 import pytest
 #0 celsius is 32 F
 
-def convert(t):
-    return 32
+def convert_c_to_f(t):
+    answer = t * 9/5 + 32
+    return answer
 
 def test_answer():
-    assert convert(0) == 32
+    assert convert_c_to_f(0) == 32
+    assert convert_c_to_f(5) != 32
 
 
 #temp = int(input("Please introduce temperature:"))
